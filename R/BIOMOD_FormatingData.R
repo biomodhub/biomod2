a8e011b1cdc80c95be5f02441e22488414a11961
CeresBarros <cbarros@mail.ubc.ca>
2021-10-04 14:49:48 -0700

    typo fix

--- R/BIOMOD_FormatingData.R
+++ R/BIOMOD_FormatingData.R
@@ -219,7 +219,7 @@
   if (binaryResp) {
     if (length(setdiff(resp.var, c(0, 1, NA)))) {
       cat("\n      ! Response variable is not binary, but binaryResp == TRUE... Is it really what you want?")
-      cat("\n      ! Values > 0 will be covnerted to 1s")
+      cat("\n      ! Values > 0 will be converted to 1s")
     }
     resp.var[which(resp.var>0)] <- 1
     resp.var[which(resp.var<=0)] <- 0
