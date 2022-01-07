a22aa7f1e9a5eaf28e0f8454b002b2532db52ceb
MayaGueguen <maya.gueguen@gmail.com>
2022-01-07 13:02:35 +0100

    Add documentation for bm_CVnnet function

--- R/bm_CVnnet.R
+++ R/bm_CVnnet.R
@@ -1,3 +1,47 @@
+###################################################################################################
+##' @name bm_CVnnet
+##' @author Damien Georges
+##' 
+##' @title Cross-validation for Neural Networks
+##' 
+##' @description 
+##' 
+##' This internal \pkg{biomod2} function allows the user to compute cross-validation for neural 
+##' networks in ANN model (see \code{\link[nnet]{nnet}} and \code{\link{BIOMOD_Modeling}}).
+##' 
+##' @param Input complete dataset with explanatory variables
+##' @param Target calibration dataset with observed presence / absence
+##' @param size in modeling options : ANN$size
+##' @param decay in modeling options ANN$decay
+##' @param maxit in modeling options ANN$maxit
+##' @param nbCV in modeling options ANN$nbCV
+##' @param W weights over calibration lines
+##' 
+##' 
+##' @return  
+##' 
+##' A \code{data.frame} containing the following elements :
+##' \itemize{
+##'   \item{\code{Size} : }{the size}
+##'   \item{\code{Decay} : }{the decay value}
+##'   \item{\code{AUC} : }{the corresponding Area Under Curve}
+##' }
+##' 
+##' 
+##' 
+##' @seealso \code{\link{BIOMOD_ModelingOptions}}, \code{\link{BIOMOD_Modeling}}, 
+##' \code{\link{bm_SampleBinaryVector}}
+##' 
+##' 
+##' @keywords neural networks, cross-validation
+##' 
+##' 
+##' @importFrom pROC auc roc
+##' 
+##' @export
+##' 
+##'
+###################################################################################################
 
 
 bm_CVnnet = function(Input,
@@ -8,89 +52,45 @@ bm_CVnnet = function(Input,
                      nbCV = 5,
                      W = NULL)
 {
-  #   require(pROC, quietly=T)
-  
-  Eval = data.frame(matrix(
-    0,
-    ncol = 3,
-    nrow = 16,
-    dimnames = list(NULL, c("Size", "Decay", "AUC"))
-  ))
+  ## Prepare output table
+  Eval = data.frame(matrix(0, ncol = 3, nrow = 16, dimnames = list(NULL, c("Size", "Decay", "AUC"))))
   Eval[, 1] = rep(size, 4)
   Eval[, 2] = rep(decay, each = 4)
+  
+  
   for (i in 1:nbCV) {
     set.seed(555)
-    Samp = bm_SampleBinaryVector(Target, 0.5)
+    Samp = bm_SampleBinaryVector(ref = Target, ratio = 0.5)
     
-    if (is.null(W)) {
-      Eval[, 3] = Eval[, 3] + apply(
-        Eval[, 1:2],
-        1,
-        Samp,
-        Target,
-        Input,
-        FUN = function(x, Samp, Target, Input) {
-          nn = nnet(
-            eval(parse(
-              text = paste(
-                "Target[Samp$calibration]",
-                paste(.scopeExpSyst(Input[1:10, , drop =
-                                            FALSE], "GBM"), collapse = "")
-              )
-            )),
-            data = Input[Samp$calibration, , drop = FALSE],
-            size = x[1],
-            decay = x[2],
-            maxit = maxit,
-            trace = FALSE
-          )
-          AUC = as.numeric(pROC::auc(pROC::roc(Target[Samp$evaluation], predict(nn, Input[Samp$evaluation, , drop =
-                                                                                            FALSE]))))
-          return(AUC)
-        }
-      )
-    } else{
-      Eval[, 3] =
-        Eval[, 3] +
-        apply(
-          Eval[, 1:2],
-          1,
-          Samp,
-          Target,
-          Input,
-          W,
-          FUN = function(x, Samp, Target, Input, W) {
-            nn =
-              nnet(
-                eval(parse(
-                  text = paste(
-                    "Target[Samp$calibration]",
-                    paste(.scopeExpSyst(Input[1:10, , drop = FALSE], "GBM"), collapse = "")
-                  )
-                )),
-                data = Input[Samp$calibration, , drop = FALSE],
-                weights = W[Samp$calibration],
-                size = x[1],
-                decay = x[2],
-                maxit = maxit,
-                trace = FALSE
-              )
-            AUC <-
-              as.numeric(pROC::auc(
-                pROC::roc(
-                  Target[Samp$evaluation],
-                  as.numeric(predict(nn, Input[Samp$evaluation, , drop =
-                                                 FALSE])),
-                  levels = c(0, 1),
-                  direction = '<'
-                )
-              ))
-            return(AUC)
-          }
-        )
-    }
+    W.tmp = ifelse(is.null(W), rep(1, length(Target)), W)
+    
+    
+    Eval[, 3] = Eval[, 3] + apply(Eval[, 1:2],
+                                  1,
+                                  Samp,
+                                  Target,
+                                  Input,
+                                  W.tmp,
+                                  FUN = function(x, Samp, Target, Input, W.tmp) {
+                                    nn = nnet(eval(parse(text = paste("Target[Samp$calibration]",
+                                                                      paste(.scopeExpSyst(Input[1:10, , drop = FALSE], "GBM"), collapse = "")))),
+                                              data = Input[Samp$calibration, , drop = FALSE],
+                                              weights = W.tmp[Samp$calibration],
+                                              size = x[1],
+                                              decay = x[2],
+                                              maxit = maxit,
+                                              trace = FALSE)
+                                    AUC = roc(Target[Samp$evaluation],
+                                              as.numeric(predict(nn, Input[Samp$evaluation, , drop = FALSE])),
+                                              levels = c(0, 1),
+                                              direction = '<')
+                                    AUC <- as.numeric(auc(AUC))
+                                    return(AUC)
+                                  })
   }
+  
   Eval[, 3] = Eval[, 3] / nbCV
   z = which.max(Eval[, 3])
+  
   return(Eval[z, 1:2])
 }
