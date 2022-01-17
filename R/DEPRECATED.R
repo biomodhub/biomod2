bb0ba92ed436b75aa635ffecb61a1e16278d304c
MayaGueguen <maya.gueguen@gmail.com>
2022-01-17 15:37:54 +0100

    Add DEPRECATED functions (not done yet)

---
+++ R/DEPRECATED.R
@@ -0,0 +1,38 @@
+
+##'
+##' @export
+##'
+
+BIOMOD_ConvertOldRun <- function(...) { .Deprecated() }
+BIOMOD_cv <- function(...) { .Deprecated("BIOMOD_CrossValidation") }
+BIOMOD_presenceonly <- function(...) { .Deprecated("BIOMOD_PresenceOnly") }
+BIOMOD_tuning <- function(...) { .Deprecated("BIOMOD_Tuning") }
+
+BinaryTransformation <- function(...) { .Deprecated("bm_BinaryTransformation") }
+calculate.stat <- function(...) { .Deprecated("bm_CalculateStat") }
+CustomIndexMaker <- function(...) { .Deprecated() }
+FilteringTransformation <- function(...) { .Deprecated("BinaryTransformation") }
+Find.Optim.Stat <- function(...) { .Deprecated("bm_FindOptimStat") }
+getStatOptimValue <- function(...) { .Deprecated("get_optim_value") }
+makeFormula <- function(...) { .Deprecated("bm_MakeFormula") }
+Print_Default_ModelingOptions <- function(...) { .Deprecated("bm_DefaultModelingOptions") }
+sample.factor.levels <- function(...) { .Deprecated("bm_SampleFactorLevels") }
+sre <- function(...) { .Deprecated("bm_SRE") }
+.transform.outputs.array <- function(...) { .Deprecated() }
+.transform.outputs.list <- function(...) { .Deprecated(".transform_outputs_list") }
+update_objects <- function(...) { .Deprecated() }
+variables_importance <- function(...) { .Deprecated("bm_VariablesImportance") }
+
+
+# DF_to_ARRAY
+# evaluate
+# full_suffling
+# level.plot
+# models_scores_graph
+# multiple.plot
+# ProbDensFunc
+# randomise_data
+# response.plot
+# response.plot2
+# SampleMat2
+# zzz_bm
