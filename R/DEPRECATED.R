96f0536c0b4a6850d0db58725508be167c69ddf4
CeresBarros <cbarros@mail.ubc.ca>
2022-04-19 14:10:07 -0700

    more CRAN error fixes: all DEPRECATED must be exported

--- R/DEPRECATED.R
+++ R/DEPRECATED.R
@@ -1,32 +1,157 @@
 
+##' BIOMOD_ConvertOldRun
+##'
+##' Deprecated
 ##'
 ##' @export
-##'
-
 BIOMOD_ConvertOldRun <- function(...) { .Deprecated() }
+
+##' BIOMOD_cv
+##'
+##' Deprecated, use \code{BIOMOD_CrossValidation}
+##'
+##' @export
 BIOMOD_cv <- function(...) { .Deprecated("BIOMOD_CrossValidation") }
+
+##' BIOMOD_presenceonly
+##'
+##' Deprecated, use \code{BIOMOD_PresenceOnly}
+##'
+##' @export
 BIOMOD_presenceonly <- function(...) { .Deprecated("BIOMOD_PresenceOnly") }
+
+##' BIOMOD_tuning
+##'
+##' Deprecated, use \code{BIOMOD_Tuning}
+##'
+##' @export
 BIOMOD_tuning <- function(...) { .Deprecated("BIOMOD_Tuning") }
 
+##' BinaryTransformation
+##'
+##' Deprecated, use \code{bm_BinaryTransformation}
+##'
+##' @export
 BinaryTransformation <- function(...) { .Deprecated("bm_BinaryTransformation") }
-calculate.stat <- function(...) { .Deprecated("bm_CalculateStat") }
-CustomIndexMaker <- function(...) { .Deprecated() }
-FilteringTransformation <- function(...) { .Deprecated("BinaryTransformation") }
-Find.Optim.Stat <- function(...) { .Deprecated("bm_FindOptimStat") }
-getStatOptimValue <- function(...) { .Deprecated("get_optim_value") }
-makeFormula <- function(...) { .Deprecated("bm_MakeFormula") }
-models_scores_graph <- function(...) { .Deprecated("bm_PlotEvalMean") }
-Print_Default_ModelingOptions <- function(...) { .Deprecated("bm_DefaultModelingOptions") }
-ProbDensFunc <- function(...) { .Deprecated("bm_PlotRangeSize") }
-response.plot <- function(...) { .Deprecated("bm_PlotResponseCurves") }
-response.plot2 <- function(...) { .Deprecated("bm_PlotResponseCurves") }
-sample.factor.levels <- function(...) { .Deprecated("bm_SampleFactorLevels") }
-sre <- function(...) { .Deprecated("bm_SRE") }
-.transform.outputs.array <- function(...) { .Deprecated() }
-.transform.outputs.list <- function(...) { .Deprecated(".transform_outputs_list") }
-update_objects <- function(...) { .Deprecated() }
-variables_importance <- function(...) { .Deprecated("bm_VariablesImportance") }
 
+##' calculate.stat
+##'
+##' Deprecated, use \code{bm_CalculateStat}
+##'
+##' @export
+calculate.stat <- function(...) { .Deprecated("bm_CalculateStat") }
+
+##' CustomIndexMaker
+##'
+##' Deprecated
+##'
+##' @export
+CustomIndexMaker <- function(...) { .Deprecated() }
+
+##' FilteringTransformation
+##'
+##' Deprecated, use \code{BinaryTransformation}
+##'
+##' @export
+FilteringTransformation <- function(...) { .Deprecated("BinaryTransformation") }
+
+##' Find.Optim.Stat
+##'
+##' Deprecated, use \code{bm_FindOptimStat}
+##'
+##' @export
+Find.Optim.Stat <- function(...) { .Deprecated("bm_FindOptimStat") }
+
+##' getStatOptimValue
+##'
+##' Deprecated, use \code{get_optim_value}
+##'
+##' @export
+getStatOptimValue <- function(...) { .Deprecated("get_optim_value") }
+
+##' makeFormula
+##'
+##' Deprecated, use \code{bm_MakeFormula}
+##'
+##' @export
+makeFormula <- function(...) { .Deprecated("bm_MakeFormula") }
+
+##' models_scores_graph
+##'
+##' Deprecated, use \code{bm_PlotEvalMean}
+##'
+##' @export
+models_scores_graph <- function(...) { .Deprecated("bm_PlotEvalMean") }
+
+##' Print_Default_ModelingOptions
+##'
+##' Deprecated, use \code{bm_DefaultModelingOptions}
+##'
+##' @export
+Print_Default_ModelingOptions <- function(...) { .Deprecated("bm_DefaultModelingOptions") }
+
+##' ProbDensFunc
+##'
+##' Deprecated, use \code{bm_PlotRangeSize}
+##'
+##' @export
+ProbDensFunc <- function(...) { .Deprecated("bm_PlotRangeSize") }
+
+##' response.plot
+##'
+##' Deprecated, use \code{bm_PlotResponseCurves}
+##'
+##' @export
+response.plot <- function(...) { .Deprecated("bm_PlotResponseCurves") }
+
+##' response.plot2
+##'
+##' Deprecated, use \code{bm_PlotResponseCurves}
+##'
+##' @export
+response.plot2 <- function(...) { .Deprecated("bm_PlotResponseCurves") }
+
+##' sample.factor.levels
+##'
+##' Deprecated, use \code{bm_SampleFactorLevels}
+##'
+##' @export
+sample.factor.levels <- function(...) { .Deprecated("bm_SampleFactorLevels") }
+
+##' sre
+##'
+##' Deprecated, use \code{bm_SRE}
+##'
+##' @export
+sre <- function(...) { .Deprecated("bm_SRE") }
+
+##' .transform.outputs.array
+##'
+##' Deprecated
+##'
+##' @export
+.transform.outputs.array <- function(...) { .Deprecated() }
+
+##' .transform.outputs.list
+##'
+##' Deprecated, use \code{.transform_outputs_list}
+##'
+##' @export
+.transform.outputs.list <- function(...) { .Deprecated(".transform_outputs_list") }
+
+##' update_objects
+##'
+##' Deprecated
+##'
+##' @export
+update_objects <- function(...) { .Deprecated() }
+
+##' variables_importance
+##'
+##' Deprecated, use \code{bm_VariablesImportance}
+##'
+##' @export
+variables_importance <- function(...) { .Deprecated("bm_VariablesImportance") }
 
 # DF_to_ARRAY
 # evaluate
