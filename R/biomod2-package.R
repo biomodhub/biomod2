69c907cf71870113a9ce8de4186c2899adc84ada
rpatin <rpatin@github.com>
2022-11-16 14:38:35 +0100

    update version number to 4.2-1

--- R/biomod2-package.R
+++ R/biomod2-package.R
@@ -10,7 +10,7 @@
   {
     RFver <- read.dcf(file = system.file("DESCRIPTION", package = pkgname), fields = "Version")
     mess <- paste(pkgname, RFver, "loaded.\n")
-    mess <- paste(mess, "/!\\ Update 4.2-0 now relies on terra version >= 1.6.33. Make sure to update with `devtools::install_github('rspatial/terra')`. We apologize for the trouble >{o.o}<")
+    mess <- paste(mess, "/!\\ Since version 4.2 biomod2 relies on terra version >= 1.6.33. Make sure to update with `devtools::install_github('rspatial/terra')`. We apologize for the trouble >{o.o}<")
     packageStartupMessage(mess)
   }
 }
